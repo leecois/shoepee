a9a722c9d16215ee012040842b9dfa71b6cb8b90
leecois <141781398+leecois@users.noreply.github.com>
2023-10-02 11:41:50 +0700

    Add Navigation

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Navigation from './customer/components/Navigation/Navigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation />
+      <div>
+        <h1>Home</h1>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
--- frontend/store/app/(routes)/page.tsx
+++
@@ -1,7 +0,0 @@
-const HomePage = () => {
-    return ( 
-        <div>Hello Store</div>
-    );
-}
-
-export default HomePage ;
