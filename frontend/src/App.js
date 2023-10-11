50f33db37ee1e985b8c05fb1fec0250d8038101d
Do Hoang Anh <dohoanganh.official@gmail.com>
2023-10-11 21:52:06 +0700

    Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Navigation from "./customer/components/Navigation/Navigation";
 import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/footer";
 import "./App.css";
+import SignIn from "./customer/Auth/Sign-in";
 
 function App() {
   return (
