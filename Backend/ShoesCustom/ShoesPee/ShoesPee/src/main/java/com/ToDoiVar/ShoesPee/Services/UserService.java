a480ea9781ea90192c075a5dacc86d93be818685
anxemer <anxemer224@gmail.com>
2023-10-11 11:23:27 +0700

    anxem update

--- Backend/ShoesCustom/ShoesPee/ShoesPee/src/main/java/com/ToDoiVar/ShoesPee/Services/UserService.java
+++ Backend/ShoesCustom/ShoesPee/ShoesPee/src/main/java/com/ToDoiVar/ShoesPee/Services/UserService.java
@@ -1,13 +1,14 @@
 package com.ToDoiVar.ShoesPee.Services;
 
-import com.ToDoiVar.ShoesPee.Models.Brand;
 import com.ToDoiVar.ShoesPee.Models.User;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 public interface UserService {
-    public List<User> readAll();
-    User saveUser(User user);
+    public List<User> getAllUser();
+    public User createUser(User user);
     User getUserById(Long id);
     void removeUser(Long id);
+    User editUser(Long id,User newUser);
+    
+
 }
