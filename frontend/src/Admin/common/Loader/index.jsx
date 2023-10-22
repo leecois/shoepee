608a5063c7d371c87f1093cf58672dde5542ec63
leecois <141781398+leecois@users.noreply.github.com>
2023-10-23 01:44:22 +0700

    Update scene of nike 3d

--- frontend/src/Admin/common/Loader/index.jsx
+++ frontend/src/Admin/common/Loader/index.jsx
@@ -1,10 +1,9 @@
 const Loader = () => {
-    return (
-      <div className="flex h-screen items-center justify-center bg-white">
-        <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
-      </div>
-    );
-  };
-  
-  export default Loader;
-  
+  return (
+    <div className="flex h-screen items-center justify-center dark:border-strokedark dark:bg-boxdark ">
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+    </div>
+  );
+};
+
+export default Loader;
--- frontend/src/customer/components/Scene/Scene.jsx
+++ frontend/src/customer/components/Scene/Scene.jsx
@@ -1,7 +1,8 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls} from "@react-three/drei";
 import { Suspense } from "react";
-import ModelShoe from "./models/ModelShoe.jsx";
+import ModelShoe from "./models/ModelShoe";
+import ModelNike from "./models/ModelNike";
 
 function Scene() {
   return (
@@ -9,7 +10,7 @@ function Scene() {
       <Suspense fallback={<p>Loading...</p>} />
       <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
       <directionalLight intensity={0.1} position={[100, 0, 50]} />
-      <ModelShoe />
+      <ModelNike />
       <Environment files="./img/small.hdr" background />
       <Suspense />
     </Canvas>
