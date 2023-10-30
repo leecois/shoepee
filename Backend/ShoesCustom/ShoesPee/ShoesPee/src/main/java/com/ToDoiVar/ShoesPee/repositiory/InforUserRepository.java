bcbbf186e90c52294fadb121d90a210fcd43a6f8
minhchan <minhtran10723@gmail.com>
2023-10-30 21:44:24 +0700

    add inforuser

---
+++ Backend/ShoesCustom/ShoesPee/ShoesPee/src/main/java/com/ToDoiVar/ShoesPee/repositiory/InforUserRepository.java
@@ -0,0 +1,15 @@
+package com.ToDoiVar.ShoesPee.repositiory;
+
+import com.ToDoiVar.ShoesPee.Models.InforUser;
+import com.ToDoiVar.ShoesPee.Models.ShoeModels;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InforUserRepository extends JpaRepository<InforUser,Integer> {
+
+
+    Optional<List<InforUser>> getInforUserById(int id);
+
+}
