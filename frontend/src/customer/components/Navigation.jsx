05c1d3cf2feeb60c9518a8959a98383385d52d4c
leecois <141781398+leecois@users.noreply.github.com>
2023-10-30 07:12:47 +0700

    Design again navigation

--- frontend/src/customer/components/Navigation.jsx
+++ frontend/src/customer/components/Navigation.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { cartItemsCountSelector } from '../../containers/Cart/selectors';
+import AuthModal from '../../Authentication/AuthModal';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -15,6 +16,10 @@ export default function Navigation() {
   const cartItemsCount = useSelector(cartItemsCountSelector);
   const [open, setOpen] = useState(false);
   const [openAuthModal, setOpenAuthModal] = useState(false);
+  const [signedIn, setSignedIn] = useState(false);
+
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
 
   // Function to open the sign-in modal
   const handleClickOpen = () => {
@@ -24,86 +29,185 @@ export default function Navigation() {
   // Function to close the sign-in modal
   const handleClose = () => {
     setOpenAuthModal(false);
+    setSignedIn(true);
   };
 
+  // Function to handle scroll events
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos, visible]);
+
   return (
-    <div className="navbar bg-base-200">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <>
+      <div
+        className={`navbar bg-base-200 sticky top-0 z-50 transition-all duration-300 ${
+          visible ? 'opacity-100' : 'opacity-0 -translate-y-full'
+        }`}
+      >
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>Shop</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-          </ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/products">Shop</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <button className="btn btn-ghost font-bold normal-case text-xl">
+            SHOEPEE
+          </button>
+        </div>
+
+        {/* RIGHT */}
+        <div className="navbar-end gap-4 flex-none">
+          {/* SIGN IN */}
+          {signedIn ? (
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div className="dropdown dropdown-end">
+              <label
+                tabIndex={0}
+                className="btn btn-ghost btn-circle"
+                onClick={handleClickOpen}
+              >
+                Sign in
+              </label>
+            </div>
+          )}
+          {/* CART */}
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">
+                  {cartItemsCount}
+                </span>
+              </div>
+            </label>
+            <div
+              tabIndex={0}
+              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+            >
+              <div className="card-body">
+                <span className="font-bold text-lg">
+                  {cartItemsCount} Items
+                </span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={handleCartClick}
+                  >
+                    View cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="navbar-center">
-        <button className="btn btn-ghost font-bold normal-case text-xl">
-          SHOEPEE
-        </button>
-      </div>
-      <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
+      <img
+        src="https://images.vans.com/is/image/VansBrand/clp-hero-top?$fullres$"
+        alt="Vans"
+      />
+      {openAuthModal && (
+        <div className="modal active">
+          <div className="modal-overlay"></div>
+          <div className="modal-container">
+            <div className="modal-header">
+              <button
+                className="btn btn-clear float-right"
+                onClick={handleClickOpen}
+              ></button>
+              <div className="modal-title h5">Sign in</div>
+            </div>
+            <div className="modal-body">
+              <p>Sign in form goes here.</p>
+            </div>
+            <div className="modal-footer">
+              <button className="btn" onClick={handleClose}>
+                Close
+              </button>
+            </div>
           </div>
-        </button>
-      </div>
-    </div>
+          <AuthModal
+            handleClickOpen={openAuthModal}
+            handleClose={handleClose}
+          />
+        </div>
+      )}
+    </>
   );
 }
